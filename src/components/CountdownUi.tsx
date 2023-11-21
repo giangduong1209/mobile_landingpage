b058614987be967cbea0d19ad66c4e5a438ad63c
chainman2209 <chainsawman2209@gmail.com>
2023-11-21 12:55:33 +0700

    feat: countdown timer

--- src/components/CountdownUi.tsx
+++ src/components/CountdownUi.tsx
@@ -1,18 +1,35 @@
 "use client";
-import { useCountdown } from "@/hooks/useCountDown";
+import Countdown from "react-countdown";
+// import { useCountdown } from "@/hooks/useCountDown";
 import React from "react";
 
 const CountdownUi = () => {
-  const DISCOUNT_DAYS = new Date("2023-12-31");
-  const [days, hours, minutes, seconds] = useCountdown<Date>(DISCOUNT_DAYS);
+  // const DISCOUNT_DAYS = new Date("2023-12-31");
+  // const [hours, minutes, seconds] = useCountdown<Date>(DISCOUNT_DAYS);
+  const renderer = ({ hours, minutes, seconds, completed }: any) => {
+    console.log("first");
+    if (completed) {
+      // Render a completed state
+      return <Countdown date={Date.now() + 43200000} renderer={renderer} />;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {hours}:{minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+
   return (
     <div className="absolute bottom-[27%] right-0 text-sm md:bottom-[15%] md:right-[20%] md:text-xl text-white font-bold">
       <span>Thời hạn ưu đãi còn: </span>
-      <span>
-        {+days + +hours + +minutes + +seconds >= 0
-          ? `${days} ngày:${hours} giờ:${minutes} phút:${seconds} giây`
+      <Countdown date={Date.now() + 43200000} renderer={renderer} />
+      {/* <span>
+        {+hours + +minutes + +seconds >= 0
+          ? `${hours} giờ:${minutes} phút:${seconds} giây`
           : 0}
-      </span>
+      </span> */}
     </div>
   );
 };
